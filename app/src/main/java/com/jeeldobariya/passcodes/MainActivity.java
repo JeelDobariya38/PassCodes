dda38a5b94a69a1db419272fae3b6ea5a5cb41de
Jeel Dobariya <jeeldobariya38@gmail.com>
2024-08-04 14:21:52 +0530

    Made a Credit Activity

--- app/src/main/java/com/jeeldobariya/passcodes/MainActivity.java
+++ app/src/main/java/com/jeeldobariya/passcodes/MainActivity.java
@@ -1,14 +1,10 @@
 package com.jeeldobariya.passcodes;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 import com.jeeldobariya.common.Logging;
-import com.jeeldobariya.common.UrlOpener;
 
 public class MainActivity extends Activity {
 
@@ -16,18 +12,19 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-	Logging.log("App Started!!");
+    Logging.log("App Started!!");
   }
 
-  public void handleRepoBtnClick(View view) {
-    new UrlOpener(this, "https://github.com/JeelDobariya38/PassCodes/releases");
+  public void onClickPasswordBtn(View view) {
+    Logging.log("Under development feature!!!");
   }
 
-  public void handleLicenseBtnClick(View view) {
-    new UrlOpener(this, "https://github.com/JeelDobariya38/PassCodes/blob/main/LICENSE.txt");
+  public void onClickCreditBtn(View view) {
+    Intent credit = new Intent(this, CreditActivity.class);
+    startActivity(credit);
   }
 
-  public void handleQuitBtnClick(View view) {
+  public void onClickQuitBtn(View view) {
     finishAndRemoveTask();
   }
 }
